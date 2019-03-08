6b32346b8b9ff5caa73563868d821b11386b5cf0
Iván Sánchez <mazyvan@hotmail.com>
2019-03-08 11:38:04 -0600

    update to match api requirements

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 var port = process.env.PORT || 5000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+app.get("/jwt-decode.min.js", function(req, res) {
+  res.sendFile(__dirname + "/jwt-decode.min.js");
+});
+
+io.on("connection", function(socket) {
+  socket.on("chat message", function(msg) {
+    io.emit("chat message", msg);
   });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function() {
+  console.log("listening on *:" + port);
 });
